7a84c3884283551db6b208fd0dda0ad677ec8099
Guillermo Sotelo <guille.sotelo.cloud@gmail.com>
2025-02-04 22:00:09 +0100

    test-prod

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -5,33 +5,22 @@ import { contactEmail, newBookingClient, newBookingAdmin, cancelBooking } from "
 const sendEmail = async (to: string, subject: string, html: string) => {
     try {
         const transporter = await transporterPromise
-
-        await new Promise((resolve, reject) => {
-            transporter.sendMail({
-                from: `"Audiología MEF" <${process.env.EMAIL}>`,
-                to,
-                subject,
-                html,
-            }, (err: any, info: any) => {
-                if (err) {
-                    console.error(err);
-                    reject(err);
-                } else {
-                    console.log(info);
-                    resolve(info);
-                }
-            })
-        })
+        await transporter.sendMail({
+            from: `"Audiología MEF" <${process.env.EMAIL}>`,
+            to,
+            subject,
+            html,
+        });
     } catch (err) {
         console.error("Error sending email:", err);
     }
 };
 
 // Exposed email functions
-export const sendContactEmail = (data: dataObj) => sendEmail("audiologia.mef@gmail.com", "Tenés un nuevo mensaje", contactEmail(data));
-export const sendNewBookingClient = (data: dataObj) => sendEmail(data.email, data.isUpdate ? "Modificaciones en el turno" : "¡Turno confirmado!", newBookingClient(data));
-export const sendNewBookingAdmin = (data: dataObj) => sendEmail("audiologia.mef@gmail.com", data.isUpdate ? "Modificaciones en el turno" : "¡Nuevo turno confirmado!", newBookingAdmin(data));
-export const sendCancelBooking = (data: dataObj) => {
-    sendEmail("audiologia.mef@gmail.com", "Turno cancelado", cancelBooking(data))
-    sendEmail(data.email, "Turno cancelado", cancelBooking(data))
+export const sendContactEmail = async (data: dataObj) => await sendEmail("audiologia.mef@gmail.com", "Tenés un nuevo mensaje", contactEmail(data));
+export const sendNewBookingClient = async (data: dataObj) => await sendEmail(data.email, data.isUpdate ? "Modificaciones en el turno" : "¡Turno confirmado!", newBookingClient(data));
+export const sendNewBookingAdmin = async (data: dataObj) => await sendEmail("audiologia.mef@gmail.com", data.isUpdate ? "Modificaciones en el turno" : "¡Nuevo turno confirmado!", newBookingAdmin(data));
+export const sendCancelBooking = async (data: dataObj) => {
+    await sendEmail("audiologia.mef@gmail.com", "Turno cancelado", cancelBooking(data))
+    await sendEmail(data.email, "Turno cancelado", cancelBooking(data))
 }
